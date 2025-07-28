9ff790fcd839b1ae2e621ac715f6067dca80375e
JamesCoding2723 <122242140+JamesCoding2723@users.noreply.github.com>
2025-07-27 21:05:03 -0700

    tracking stuff

--- src/odom.cpp
+++ src/odom.cpp
@@ -12,17 +12,25 @@ double y = 0;
 double tracking(){
     vertical_encoder.set_position(0);   
 
-    double prevdis = 0;
+    double prevdisX = 0;
+    double prevdisY = 0;
 
     while (true) {
         double heading = std::fmod(360 - imu.get_heading() + start_heading, 360);
+        double dis_travY = vertical_encoder.get_position() * 0.0002399827721;
+        //double dis_travX = horizontal_encoder.get_position() * 0.0002399827721;
+        double deltaposY = dis_travY - prevdisY;
+        //double deltaposX = dis_travX - prevdisX;
 
-        double dis_trav = vertical_encoder.get_position() * 0.0002399827721;
-        double deltapos = dis_trav - prevdis;
-
+        // x += deltaposX * std::cos(heading * (M_PI / 180)) - deltaposY * std::sin(heading * (M_PI / 180));
+        // y += deltaposX * std::sin(heading * (M_PI / 180)) + deltaposY * std::cos(heading * (M_PI / 180));
         
+        x += deltaposY * std::cos(heading * (M_PI / 180));
+        y += deltaposY * std::sin(heading * (M_PI / 180));
+        
+        return x;
+        return y;
 
-
-
+        vex::this_thread::sleep_for(10);
     }
 }
