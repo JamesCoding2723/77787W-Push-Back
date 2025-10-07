4c083004e897aed7b64c446dfbbda69ac71fd030
JamesCoding2723 <122242140+JamesCoding2723@users.noreply.github.com>
2025-10-06 19:40:52 -0700

    more auton stuff

--- src/main.cpp
+++ src/main.cpp
@@ -85,34 +85,35 @@ void autonomous()
     //LEFT 7BALL CODE
     setintakespd(-100);
     pidMoveold(14, 0.5, 200, 50);
-    pidTurnRel(-31.5, 2, 200);
-    pidMoveold(12.5, 0.5, 200, 15); // go into the 3 blocks
+    pidTurnRel(-30, 2, 200);
+    pidMoveold(12.2, 0.5, 200, 15); // go into the 3 blocks
     setintakespd(0);
     setintakerspd(0);
     //pros::delay(2000);
 
-    pidTurnRel(-87, 1, 50);
+    pidTurnRel(-90, 1, 50);
+    pros::delay(200);
     jeminrooft();
-    moveForSec(30, false, 1.15); // go to middle goal
+    moveForSec(30, false, 1.3); // go to middle goal
     setintakerspd(-100); //scoring
     pros::delay(2000);
     setintakerspd(0);
 
     setintakerspd(20);
-    pidMoveold(50, 0.5, 200, 60); //going to mtach load
+    pidMoveold(49.5, 0.5, 200, 60); //going to mtach load
     setintakerspd(0);
-    pidTurnRel(-45, 2, 50);
+    pidTurnRel(-44, 2, 50);
     //moveForSec(30, false, 0.3);
     pros::delay(500);
     jeminmecht();
     jeminrooft();
     pros::delay(100);
     setintakespd(-100);
-    moveForSec(40, true, 0.6); //collecting match load
+    moveForSec(80, true, 0.6); //collecting match load
     pros::delay(200);
-    moveForSec(30, false, 0.2);
+    moveForSec(60, false, 0.2);
     pros::delay(200);
-    moveForSec(30, true, 0.3);
+    moveForSec(60, true, 0.3);
     pros::delay(800);
 
     setintakespd(0);
